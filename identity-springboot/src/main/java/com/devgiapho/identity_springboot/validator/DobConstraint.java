ed7aa1547a2bfd6d5c472e76421d344196a593e5
Ɠia Pɦo Huỳnh <huynhgiapho1234@gmail.com>
2025-03-03 11:11:48 +0700

    Refactor

--- identity-springboot/src/main/java/com/devgiapho/identity_springboot/validator/DobConstraint.java
+++ identity-springboot/src/main/java/com/devgiapho/identity_springboot/validator/DobConstraint.java
@@ -2,15 +2,11 @@ package com.devgiapho.identity_springboot.validator;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import jakarta.validation.constraints.Size;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({FIELD})
